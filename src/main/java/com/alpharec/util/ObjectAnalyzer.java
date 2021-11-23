d8394b261314f67ac2849e244940e1f129c2ade2
neovic <16205111@hdu.edu.cn>
2021-11-23 22:01:40 +0800

    SubList,getTopN added

--- src/main/java/com/alpharec/util/ObjectAnalyzer.java
+++ src/main/java/com/alpharec/util/ObjectAnalyzer.java
@@ -1,5 +1,12 @@
 package com.alpharec.util;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.min;
+
 public class ObjectAnalyzer {
     public static <T> String ToString(T t) {
         StringBuilder s = new StringBuilder();
@@ -23,5 +30,24 @@ public class ObjectAnalyzer {
 
     public static <T> int HashCode(T t) {
         return ToString(t).hashCode();
+    }
+
+    public static <T> List<T> getTopN(int n, List<T> list, Comparator<T> comparator) {
+        if (list == null || list.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return list.stream().sorted(comparator)
+                .limit(n).collect(Collectors.toList());
+    }
+
+    public static <T> List<List<T>> SubList(List<T> list, int subSize) {
+        List<List<T>> r = new ArrayList<>();
+        if (list == null || list.isEmpty()) {
+            return r;
+        }
+        for (int i = 0; i < list.size(); i += subSize) {
+            r.add(list.subList(i, min(i + subSize, list.size())));
+        }
+        return r;
     }
 }
--- src/test/java/com/alpharec/util/TestUtil.java
+++ src/test/java/com/alpharec/util/TestUtil.java
@@ -1,9 +1,21 @@
 package com.alpharec.util;
 
+import com.alpharec.JavaConfig;
+import com.alpharec.data.Resource;
+import com.alpharec.item.MovieItem;
 import com.alpharec.pojo.Link;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.alpharec.util.ObjectAnalyzer.*;
 
 public class TestUtil {
     private static final Logger logger = LoggerFactory.getLogger(TestUtil.class);
@@ -17,5 +29,32 @@ public class TestUtil {
     public void testToString() {
         Link link = new Link(1, 2, 3);
         System.out.println(link);
+    }
+
+    @Test
+    public void testSubList() {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(i);
+        }
+        for (var i : SubList(list, 14)) {
+            logger.info("[INFO]:subList:{}, size:{}", i, i.size());
+        }
+    }
+
+    @Test
+    public void testTopN() {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(i);
+        }
+        //排序函数无意义，仅仅是为了检查是否可传入自定义排序函数
+        System.out.println(getTopN(10, list, (a, b) -> {
+            if (a * b > 10) {
+                return Integer.compare(a, b);
+            } else {
+                return Integer.compare(b, a);
+            }
+        }));
     }
 }
